bcd9adfc1898dd3184bd1aaae8725c801ded495f
yachendrakumar6 <yachendrayadav@gmail.com>
2026-01-24 15:38:48 +0000

    adding file null_handling

---
+++ Note Books/Python Scripts/Null Handling/null_handling.py
@@ -0,0 +1,75 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, coalesce
+
+spark = SparkSession.builder.appName("NullHandling").getOrCreate()
+
+data = [
+    (1, "Alice", "IT", 90000, None),
+    (2, "Bob", None, None, "Bangalore"),
+    (3, None, "HR", 60000, None),
+    (4, "David", "IT", None, "Hyderabad"),
+    (5, None, None, None, None)
+]
+
+columns = ["emp_id", "name", "dept", "salary", "location"]
+
+df = spark.createDataFrame(data, columns)
+display(df)
+
+# COMMAND ----------
+
+display(df.na.fill("UNKNOWN"))
+
+# COMMAND ----------
+
+display(
+    df.na.fill({
+    "name": "UNKNOWN",
+    "dept": "NOT KNOWN",
+    "salary": 0,
+    "location": "NULL"
+})
+)
+
+
+# COMMAND ----------
+
+display(df.na.drop())
+
+display(df.na.drop(how="all"))
+
+display(df.na.drop(subset=["salary", "dept"]))
+
+# how="any" → default
+# how="all" → only fully NULL rows dropped
+
+# COMMAND ----------
+
+display(df.na.replace(
+    to_replace={"IT": "Information Technology", "HR": "Human Resources"}
+))
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, coalesce
+
+df.select(
+    "emp_id",
+    coalesce(
+        col("location"),
+        col("dept"),
+        col("name"),
+        col("emp_id").cast("string")
+    ).alias("fallback_value")
+).show()
+
+
+# COMMAND ----------
+
+df.filter(col("salary").isNull()).show()
+
+# COMMAND ----------
+
+display(df.filter(col("salary").isNotNull()))
