57cd64e3f125ba5448dd63298892c9802cb42919
yachendrakumar6 <yachendrayadav@gmail.com>
2026-01-29 01:28:16 +0000

    cahnges made for array_tranforsmations file

--- notebooks/python_scripts/pratice_notebook.py
+++ notebooks/python_scripts/pratice_notebook.py
@@ -1,19 +1,28 @@
 # Databricks notebook source
+from pyspark.sql.functions import col
 data = [
     (1, "Alice", 29, "IT", 50000),
     (2, "Bob", 35, "HR", 60000),
     (3, "Charlie", 40, "IT", 80000),
     (4, "David", 23, "Finance", 45000),
     (5, "Eva", 31, "HR", 70000),
-    (6, "Frank", "HR", 45, 55000),
-    (7, "Grace", "Finance", 38, 75000),
-    (8, "Helen", "IT", 26, 65000),
-    (9, "Ian", "HR", 31, 52000),
-    (10, "Jack", "IT", 50, 100000)
+    (6, "Frank", 45, "HR", 55000),
+    (7, "Grace", 38, "Finance", 75000),
+    (8, "Helen", 26, "IT", 65000),
+    (9, "Ian", 31, "HR", 52000),
+    (10, "Jack", 50, "IT", 100000)
 ]
 
 cols = ["id", "name", "age", "dept", "salary"]
 df = spark.createDataFrame(data, cols)
 
-df_multi =  df.filter(df.age > 30).where(df.dept == "IT").orderBy(df.age.desc()).limit(2)
-df_multi.show()
+# display(df)
+
+# df_multi =  df.filter(df.dept == "IT").where(df.age > 25).orderBy(df.age.desc())
+# display(df_multi)
+
+df_multi1 = df.select(
+    df.id, 
+    col("age") > 25
+)
+display(df_multi1)
